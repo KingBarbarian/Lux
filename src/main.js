b58bfbb2f04ed328ca4e41b9c6c3819cefbfc7cc
GitNiko <galaxis.ling@gmail.com>
2017-09-28 17:15:15 +0800

    fix

--- src/main.js
+++ src/main.js
@@ -1,8 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { Route, Switch, HashRouter as Router } from "react-router-dom";
 import { Provider } from "react-redux";
-import { createStore, applyMiddleware } from "redux";
 import Routers from "@/routers";
 import configureStore from "./stores/configureStore";
 
