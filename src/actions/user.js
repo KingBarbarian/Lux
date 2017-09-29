b069097899ba5238a14e481ed0319526ade5ade9
GitNiko <galaxis.ling@gmail.com>
2017-09-29 14:30:46 +0800

    fix

--- src/actions/user.js
+++ src/actions/user.js
@@ -4,7 +4,10 @@ class User {
   USER_TOKEN = createRequestTypes("USER_TOKEN");
   getToken() {
     return {
-      type: this.USER_TOKEN.REQUEST
+      type: this.USER_TOKEN.REQUEST,
+      host: "http://file.dev.nongfenqi.com",
+      endpoint: "/group/1231231",
+      method: "GET"
     };
   }
 }
