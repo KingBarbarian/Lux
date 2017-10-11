ba620977e4896c809facf1f2a60244e40569ad85
weijianwei <weijianwei@nongfenqi.com>
2017-10-11 15:20:35 +0800

    测试

--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
 import Routers from "@/routers";
-import configureStore from "./stores/configureStore"
+import configureStore from "./stores/configureStore";
 
 ReactDOM.render(
   <Provider store={configureStore()}>
