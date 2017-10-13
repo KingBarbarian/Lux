561ade17e94f802f138c2a46a47b847cf7774b1c
weijianwei <weijianwei@nongfenqi.com>
2017-10-13 09:18:53 +0800

    品种选项

--- src/page/distributor/MachineSelect.js
+++ src/page/distributor/MachineSelect.js
@@ -15,22 +15,26 @@ const machinesSelector = createPaginateSelector("machines");
 @modal()
 class MachineSelect extends React.Component {
   onEndReached = ({ value = {} }) => {
-    this.props.dispatch(Distributor.machinesPaginator.loadNext(this.handleValue(value)));
+    this.props.dispatch(
+      Distributor.machinesPaginator.loadNext(this.handleValue(value))
+    );
   };
 
   onRefresh = ({ value = {} }) => {
-    this.props.dispatch(Distributor.machinesPaginator.refresh(this.handleValue(value)));
+    this.props.dispatch(
+      Distributor.machinesPaginator.refresh(this.handleValue(value))
+    );
   };
 
-  handleValue = (value) => {
-    let params = {}
-    if (!Array.isArray(value)) return params
-    const keyword = value.find(item => item.id === "keyword")
+  handleValue = value => {
+    let params = {};
+    if (!Array.isArray(value)) return params;
+    const keyword = value.find(item => item.id === "keyword");
     if (keyword) {
-      params['fuzzyAgrMachineNames'] = keyword.value
+      params["fuzzyAgrMachineNames"] = keyword.value;
     }
-    return params
-  }
+    return params;
+  };
 
   renderRow = (rowData, sectionID, rowID) => {
     return (
